c06ea0c1ba749b9eb10556ec06f6fa98a5f6aa5e
Pramod JM <pramodjm4@gmail.com>
2026-01-11 11:57:20 +0530

    fix the email email_addresses spelling

--- src/config/inngest.ts
+++ src/config/inngest.ts
@@ -12,10 +12,10 @@ const syncUser=inngest.createFunction(
     {event:'user.created'},
     async({event,step})=>{
         await connectMongooseDB();
-        const {id,email_address,first_name,last_name,image_url}=event.data;
+        const {id,email_addresses,first_name,last_name,image_url}=event.data;
         const newUser={
             clerkID:id, 
-            email:email_address[0]?.email_address,
+            email:email_addresses?.[0]?.email_address,
             firstName:first_name,
             lastName:last_name,
             imageUrl:image_url,
